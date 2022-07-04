7cbdf30f7b08cfcde2f6699f46c0afe424dafb8b
cdseo <tjckdejr1@gmail.com>
2022-07-04 15:34:36 +0900

    Feat : validate function of email phonenumber

--- GenerateAccount.m
+++ GenerateAccount.m
@@ -1,17 +1,23 @@
-function [isGenerated, stateMsg, newAccountStruct] = GenerateAccount(id, password, name, tier, prevAccountStruct)
+function [isGenerated, stateMsg, newAccountStruct] = GenerateAccount(id, password, name, tier, affiliation, emailName, emailDomain, phoneNumber, prevAccountStruct)
     % isGenerated = GenerateAccount(id, password, tier)
     % id : string
     % password : string
     % name : string
     % tier : string
+    % affiliation : string
+    % email : string
+    % phone : string
     
     isGenerated = false;
     newAccountStruct = '';
     if isempty(id); stateMsg = 'Please enter your ID'; return; end
-
     if isempty(password); stateMsg = 'Please enter your Password'; return; end
-
     if isempty(name); stateMsg = 'Please enter your Name'; return; end
+    if isempty(tier); stateMsg = 'Please enter your Tier'; return; end
+    if isempty(affiliation); stateMsg = 'Please enter your Affiliation'; return; end
+    if isempty(emailName); stateMsg = 'Please enter your E-mail name'; return; end
+    if isempty(emailDomain); stateMsg = 'Please enter your E-mail domain'; return; end
+    if isempty(phoneNumber); stateMsg = 'Please enter your Phone number'; return; end
     
     [isValid, stateMsg] = ValidateId(id);
     if ~isValid; return; end
@@ -21,6 +27,12 @@ function [isGenerated, stateMsg, newAccountStruct] = GenerateAccount(id, passwor
 
     [isValid, stateMsg] = ValidateName(name);
     if ~isValid; return; end
+
+    [isValid, stateMsg] = ValidateEmailName(emailName);
+    if ~isValid; return; end
+
+    [isValid, stateMsg] = ValidatePhoneNumber(phoneNumber);
+    if ~isValid; return; end
     
     hashedPassword = StringToHashedHex(password);
     if isfield(prevAccountStruct, id); stateMsg = 'Exist ID'; return; end
@@ -29,6 +41,10 @@ function [isGenerated, stateMsg, newAccountStruct] = GenerateAccount(id, passwor
     newAccountStruct.(id).Password = hashedPassword;
     newAccountStruct.(id).Name = name;
     newAccountStruct.(id).Tier = tier;
+    newAccountStruct.(id).Affiliation = affiliation;
+    newAccountStruct.(id).EmailName = emailName;
+    newAccountStruct.(id).EmailDomain = emailDomain;
+    newAccountStruct.(id).Phone = phoneNumber;
     isGenerated = true;
     stateMsg = 'Your account is generated successfully';
 end
---
+++ ValidatePhoneNumber.m
@@ -0,0 +1,29 @@
+function [isValid, stateMsg] = ValidatePhoneNumber(phoneNumber)
+    isValid = false;
+    stateMsg = '';
+    phoneNumberReg = '^[\+]{0,1}[0-9]+$'; % +, number
+    phoneNumberReg(strfind(phoneNumberReg, '-')) = '';
+
+    MIN_PHONENUMBER_LENGTH = 4;
+    MAX_PHONENUMBER_LENGTH = 20;
+    
+    %% Length
+    if length(phoneNumber) < MIN_PHONENUMBER_LENGTH        
+        stateMsg = sprintf('Phone number should be longer than %d words\n', MIN_PHONENUMBER_LENGTH);
+        return;
+    end
+    
+    if length(phoneNumber) > MAX_PHONENUMBER_LENGTH    
+        stateMsg = sprintf('Phone number should be shorter than %d words\n', MAX_PHONENUMBER_LENGTH);
+        return;
+    end
+
+    %% Expression
+    out = regexp(phoneNumber, phoneNumberReg, 'match');    
+    if isempty(out)
+        stateMsg = 'Invalid Phone number';
+        return;
+    end
+    
+    isValid = true;
+end
