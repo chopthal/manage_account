fad5323a9e591aa1179cc55ff96ce034ffbcdef7
cdseo <tjckdejr1@gmail.com>
2022-07-11 18:51:22 +0900

    Add : test code

---
+++ test_accessible.m
@@ -0,0 +1,35 @@
+id = 'admin';
+
+authStruct = struct;
+authStruct.Accesible = true;
+authStruct.Option1 = '';
+authStruct.Option2 = '';
+authStruct.Option3 = '';
+authStruct.Option4 = '';
+authStruct.Option5 = '';
+
+idStruct = struct;
+
+idStruct.Authority.Auth1 = authStruct; % Analysis
+idStruct.Authority.Auth2 = authStruct; % Save SPR Images
+idStruct.Authority.Auth3 = authStruct; % Autosampler Setting
+idStruct.Authority.Auth4 = authStruct; % Manage Audit-trail
+
+idStruct.Authority.Auth5 = authStruct;
+idStruct.Authority.Auth6 = authStruct;
+idStruct.Authority.Auth7 = authStruct;
+idStruct.Authority.Auth8 = authStruct;
+idStruct.Authority.Auth9 = authStruct;
+idStruct.Authority.Auth10 = authStruct;
+idStruct.Authority.Auth11 = authStruct;
+idStruct.Authority.Auth12 = authStruct;
+idStruct.Authority.Auth13 = authStruct;
+idStruct.Authority.Auth14 = authStruct;
+idStruct.Authority.Auth15 = authStruct;
+idStruct.Authority.Auth16 = authStruct;
+idStruct.Authority.Auth17 = authStruct;
+idStruct.Authority.Auth18 = authStruct;
+idStruct.Authority.Auth19 = authStruct;
+idStruct.Authority.Auth20 = authStruct;
+
+Accounts.(id) = idStruct;
