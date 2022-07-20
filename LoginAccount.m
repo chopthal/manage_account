dc7e43f5929b112efcd49a40e626bcdd3715b80b
cdseo <tjckdejr1@gmail.com>
2022-07-20 18:52:56 +0900

    Feat : Delete tier output

--- LoginAccount.m
+++ LoginAccount.m
@@ -1,35 +1,56 @@
-function [isLoggedIn, tier, stateMsg] = LoginAccount(id, password)
+function [isLoggedIn, stateMsg] = LoginAccount(id, password)
 
-isLoggedIn = false;
-tier = '';
-stateMsg = '';
+    isLoggedIn = false;
+    stateMsg = '';
+    
+    if isempty(id); stateMsg = 'Please enter your ID'; 
+    
+        return;
+    
+    end
+    
+    if isempty(password)
+        
+        stateMsg = 'Please enter your Password'; 
+        
+        return; 
+    
+    end
+    
+    Accounts = struct;
+    hashedPassword = StringToHashedHex(password);
+    
+    if isfile('accounts.mat')
+    
+        loadedVariable = load('accounts.mat');
+        Accounts = loadedVariable.Accounts;
+    
+    end
+    
+    if ~isfield(Accounts, id)
+    
+        stateMsg = 'Invalid ID';
+    
+        return;
+    
+    end
+    
+    if ~isfield(Accounts.(id), 'Password')
+    
+        stateMsg = 'Invalid Account. Please contact the Administrator!';
+    
+        return;
+    
+    end
+    
+    if isequal(hashedPassword, Accounts.(id).Password)
+    
+        isLoggedIn = true;
+        
+    else
+    
+        stateMsg = 'Invalid Password!';
 
-if isempty(id); stateMsg = 'Please enter your ID'; return; end
-if isempty(password); stateMsg = 'Please enter your Password'; return; end
-
-Accounts = struct;
-hashedPassword = StringToHashedHex(password);
-
-if isfile('accounts.mat')
-    loadedVariable = load('accounts.mat');
-    Accounts = loadedVariable.Accounts;
-end
-
-if ~isfield(Accounts, id)
-    stateMsg = 'Invalid ID';
-    return;
-end
-
-if ~isfield(Accounts.(id), 'Password')
-    stateMsg = 'Invalid Account. Please contact the Administrator!';    
-    return;
-end
-
-if isequal(hashedPassword, Accounts.(id).Password)
-    isLoggedIn = true;
-    tier = Accounts.(id).Tier;
-else
-    stateMsg = 'Invalid Password!';
-end
+    end
 
 end
